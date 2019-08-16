fab6ab8e0e7aa9ccc41b8978b739ce902d1c5e81
Miguel Angel Ake Us <miguel.ake@bestday.com>
2019-08-16 13:34:42 -0500

    Se quitan los test unitarios generados por el CLI de Angular

--- src/app/pokedex.service.spec.ts
+++ src/app/pokedex.service.spec.ts
@@ -2,11 +2,6 @@ import { TestBed } from '@angular/core/testing';
 
 import { PokedexService } from './pokedex.service';
 
-describe('PokedexService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
-
-  it('should be created', () => {
-    const service: PokedexService = TestBed.get(PokedexService);
-    expect(service).toBeTruthy();
-  });
+describe('PokedexService', () => { 
+  // Your test here.
 });
